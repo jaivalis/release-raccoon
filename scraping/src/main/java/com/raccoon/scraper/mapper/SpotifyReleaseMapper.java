ea81423811b34293145eb6a6428a84f2dd113aad
jaivalis <john@axual.io>
2022-01-21 16:41:45 +0100

    Fix date parsing issue

---
+++ scraping/src/main/java/com/raccoon/scraper/mapper/SpotifyReleaseMapper.java
@@ -0,0 +1,39 @@
+package com.raccoon.scraper.mapper;
+
+import com.raccoon.entity.Release;
+import com.wrapper.spotify.model_objects.specification.AlbumSimplified;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Makes a parses com.wrapper.spotify.model_objects.specification.AlbumSimplified to
+ * returned as search results
+ */
+@Slf4j
+@ApplicationScoped
+public class SpotifyReleaseMapper {
+
+    public Release fromAlbumSimplified(AlbumSimplified albumSimplified) {
+        final var release = new Release();
+
+        release.setName(albumSimplified.getName());
+        release.setType(albumSimplified.getAlbumType().toString());
+        release.setSpotifyUri(albumSimplified.getUri());
+
+        try {
+            release.setReleasedOn(LocalDate.parse(albumSimplified.getReleaseDate()));
+        } catch (DateTimeParseException e) {
+            log.warn("Exception occurred when parsing release date {}, falling back to today",
+                    albumSimplified.getReleaseDate());
+            release.setReleasedOn(LocalDate.now());
+        }
+
+        return release;
+    }
+
+}
---
+++ scraping/src/test/java/com/raccoon/scraper/mapper/SpotifyReleaseMapperTest.java
@@ -0,0 +1,69 @@
+package com.raccoon.scraper.mapper;
+
+import com.raccoon.entity.Release;
+import com.wrapper.spotify.enums.AlbumType;
+import com.wrapper.spotify.enums.ModelObjectType;
+import com.wrapper.spotify.model_objects.specification.AlbumSimplified;
+import com.wrapper.spotify.model_objects.specification.ArtistSimplified;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SpotifyReleaseMapperTest {
+
+    SpotifyReleaseMapper mapper = new SpotifyReleaseMapper();
+
+    @ParameterizedTest
+    @EnumSource(AlbumType.class)
+    void fromAlbumSimplified(AlbumType type) {
+        var uri = "releaseUri";
+        var releaseDate = LocalDate.now();
+        var albumSimplified = new AlbumSimplified.Builder()
+                .setId("1")
+                .setArtists(
+                        new ArtistSimplified.Builder()
+                                .setName("artistName")
+                                .setUri("artistUri")
+                                .build()
+                )
+                .setType(ModelObjectType.ALBUM)
+                .setAlbumType(type)
+                .setUri(uri)
+                .setReleaseDate(releaseDate.toString())
+                .build();
+
+        Release release = mapper.fromAlbumSimplified(albumSimplified);
+
+        assertEquals(type.toString(), release.getType());
+        assertEquals(uri, release.getSpotifyUri());
+        assertEquals(releaseDate, release.getReleasedOn());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"2020", "2020-30-30", "garbage"})
+    @DisplayName("fromAlbumSimplified(): Bad date provided, should fallback to today")
+    void fromAlbumSimplifiedBadDate(String invalidDates) {
+        var albumSimplified = new AlbumSimplified.Builder()
+                .setId("1")
+                .setArtists()
+                .setType(ModelObjectType.ALBUM)
+                .setAlbumType(AlbumType.ALBUM)
+                .setUri("releaseUri")
+                .setReleaseDate(invalidDates)
+                .build();
+
+        Release release = mapper.fromAlbumSimplified(albumSimplified);
+
+        var now = LocalDate.now();
+        assertEquals(now.getYear(), release.getReleasedOn().getYear());
+        assertEquals(now.getMonth(), release.getReleasedOn().getMonth());
+        assertEquals(now.getDayOfMonth(), release.getReleasedOn().getDayOfMonth());
+    }
+
+}
