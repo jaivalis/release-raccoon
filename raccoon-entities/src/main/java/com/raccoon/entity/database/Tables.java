d12195101f6958333d0ffc00db3c75bad63a19c7
jaivalis <jaivalis@users.noreply.github.com>
2022-08-01 09:28:40 +0200

    Sonar fixes (#48)
    * Removed commented out code
    
    * Using pull_request tag
    
    Co-authored-by: jaivalis <john@axual.io>

--- raccoon-entities/src/main/java/com/raccoon/entity/database/Tables.java
+++ raccoon-entities/src/main/java/com/raccoon/entity/database/Tables.java
@@ -5,4 +5,7 @@ public class Tables {
     public static final String ARTIST_RELEASE = "ArtistRelease";
     public static final String USER_ARTIST = "UserArtist";
 
+    private Tables() {
+        // Hiding constructor
+    }
 }
