0f86b91cde1d40580ce7a113e9fbebd29897b6aa
jaivalis <johnaivalis@gmail.com>
2025-09-08 20:58:33 +0200

    test: add `ArtistSearchResourceIT` tests for `followedByUser`
    Signed-off-by: jaivalis <johnaivalis@gmail.com>

--- release-raccoon-app/src/test/resources/import-artist-search.sql
+++ release-raccoon-app/src/test/resources/import-artist-search.sql
@@ -4,16 +4,21 @@ VALUES
     (100, 'user100@mail.com');
 
 INSERT INTO Artist
-    (name)
+    (artistId, name)
 VALUES
-    ('led zeppeling'),
-    ('Zapp Franka'),
-    ('vangelio'),
-    ('krs-two'),
-    ('me-roy'),
-    ('philip grass'),
-    ('kanye east'),
-    ('min Romeo'),
-    ('Inner Kamoze'),
-    ('Beta Blondy'),
-    ('philip stone');
+    (100, 'led zeppeling'),
+    (200, 'Zapp Franka'),
+    (300, 'vangelio'),
+    (400, 'krs-two'),
+    (500, 'me-roy'),
+    (600, 'philip grass'),
+    (700, 'kanye east'),
+    (800, 'min Romeo'),
+    (900, 'Inner Kamoze'),
+    (101, 'Beta Blondy'),
+    (102, 'philip stone');
+
+INSERT INTO UserArtist
+    (user_id, artist_id, hasNewRelease)
+VALUES
+    (100, 100, false);
